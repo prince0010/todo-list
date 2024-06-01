abd49793cdc083b5f37c3fdbb9d7d2b7beaaa049
PRINCE <princenagac12@gmail.com>
2024-06-01 11:00:49 +0800

    Update task.ts

--- src/components/task.ts
+++ src/components/task.ts
@@ -1,113 +1,117 @@
 import { action, computed, makeObservable, observable } from "mobx";
-import { TaskInterface } from "../interaces";
+import { TaskInterface } from "../interfaces";
 
-const STORAGE_KEY = '@tasks'
+const STORAGE_KEY = '@tasks';
 
 export class Task {
     [x: string]: any;
     constructor() {
-        this.fetchs()
-        makeObservable(this)
+        this.fetchs();
+        makeObservable(this);
     }
 
-    protected fetchs(){
-        this._tasks = JSON.parse(localStorage[STORAGE_KEY])
+    protected fetchs() {
+        try {
+            const storedTasks = localStorage.getItem(STORAGE_KEY);
+            this._tasks = storedTasks ? JSON.parse(storedTasks) : [];
+        } catch (error) {
+            console.error("Failed to parse tasks from localStorage:", error);
+            this._tasks = [];
+        }
     }
 
-    protected sync(){
-        localStorage[STORAGE_KEY] = JSON.stringify(this._tasks)
+    protected sync() {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(this._tasks));
     }
 
     @observable
-    protected _tasks: TaskInterface[] = []
+    protected _tasks: TaskInterface[] = [];
 
     @observable
-    protected _tasksEdit?: TaskInterface
-
+    protected _tasksEdit?: TaskInterface;
 
     @computed
-    get taskEdit(){
-        return this._tasksEdit
-
+    get taskEdit() {
+        return this._tasksEdit;
     }
 
     @computed
-     get tasks() {
-        return this._tasks.filter((task) => !task.isDone)
-        .sort((a,b) => b.updatedAt - a.updatedAt)
-     }
-     
-     @computed 
-     get completedTasks(){
-            return this._tasks.filter((task) => task.isDone)
-            .sort((a,b) => b.updatedAt - a.updatedAt)
-     }
+    get tasks() {
+        return this._tasks
+            .filter((task) => !task.isDone)
+            .sort((a, b) => b.updatedAt - a.updatedAt);
+    }
 
-     //editing and Updating
-     @action
-     edit(task: TaskInterface){
-        this._tasksEdit = task
-     }
+    @computed
+    get completedTasks() {
+        return this._tasks
+            .filter((task) => task.isDone)
+            .sort((a, b) => b.updatedAt - a.updatedAt);
+    }
 
-     protected generateId(): number {
-        let rand = Math.random()
-        while(this._tasks.find(task => task.id === rand)) {
+    // editing and Updating
+    @action
+    edit(task: TaskInterface) {
+        this._tasksEdit = task;
+    }
+
+    protected generateId(): number {
+        let rand = Math.random();
+        while (this._tasks.find(task => task.id === rand)) {
             rand = Math.random();
         }
 
         return rand;
     }
 
-    protected find(id: TaskInterface['id'], callback: (task: TaskInterface, index: number) => void){
-        const index = this._tasks.findIndex((task) => task.id === id)
-        if (index !== -1){
-            callback(this._tasks[index], index)      
+    protected find(id: TaskInterface['id'], callback: (task: TaskInterface, index: number) => void) {
+        const index = this._tasks.findIndex((task) => task.id === id);
+        if (index !== -1) {
+            callback(this._tasks[index], index);
         }
-    } 
+    }
 
-     @action
-     add(title: string){
+    @action
+    add(title: string) {
         this._tasks.push({
             title,
             id: this.generateId(),
             isDone: false,
             updatedAt: new Date().getTime()
-
-        })
-        this.sync()
-     }
+        });
+        this.sync();
+    }
 
     @action
-    update(id: TaskInterface['id'], title: string){
+    update(id: TaskInterface['id'], title: string) {
         this.find(id, (task, i) => {
             this._tasks[i] = {
                 ...task,
                 title
-            }
-            this._tasksEdit = undefined
-            this.sync()
-        })
+            };
+            this._tasksEdit = undefined;
+            this.sync();
+        });
     }
 
     @action
-    remove(id: TaskInterface['id']){
-        this.find(id , (_, i) => {
-            this._tasks.splice(i, 1) 
-            this.sync()
-        })
+    remove(id: TaskInterface['id']) {
+        this.find(id, (_, i) => {
+            this._tasks.splice(i, 1);
+            this.sync();
+        });
     }
 
     @action
-    toggleDone(id: TaskInterface['id']){
+    toggleDone(id: TaskInterface['id']) {
         this.find(id, (task, i) => {
             this._tasks[i] = {
                 ...task,
                 isDone: !task.isDone,
                 // Update when it is completed only, show tasks show in first
-                updatedAt: !task.isDone ? new Date().getTime() : task.updatedAt 
-            }
-            this.sync()
-        })
+                updatedAt: !task.isDone ? new Date().getTime() : task.updatedAt
+            };
+            this.sync();
+        });
     }
-
-    }
+}
